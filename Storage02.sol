ccdb63c63f6ad0b4409cf9df0b4f2516e6e22458
Manuel JG <mannyb1999@hotmail.com>
2024-09-20 21:06:42 -0600

    Create Storage02.sol

---
+++ Storage02.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GLP -3.0
+pragma solidity 0.8.26;
+
+
+         contract StorageCounter {
+    string public storedInfo;
+    uint public countChanges;
+
+    constructor(string memory _storedInfo){ 
+        storedInfo = _storedInfo;
+
+        function getStoredInfo() public view returns (string memory){
+        return storedInfo;
+    }
+    }
+         }
+
+    function setStoredInfo(string memory _storedInfo) public {
+        storedInfo = _storedInfo;
+
+         require(countChanges < 5, "The counter should be less than 5");
+
+         countChanges++;
+         storedInfo = _storedInfo;
+
+         
+    }
